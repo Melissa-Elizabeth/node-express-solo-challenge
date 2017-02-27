2e884b5b0641001294dc4b82922690a42269bf5d
Melissa Hare <hare.melissa@gmail.com>
2017-02-27 16:21:40 -0600

    Functionally works. Visually ugly.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,43 @@
 $(document).ready(function() {
   console.log('jquery loaded');
 
+  $.ajax({
+     type: 'GET',
+     url: '/jokes',
+     success: function(response) {
+       $('.display').text(response);
+     },
+     error: function(error){
+     }
+   });
+
+  $('.addJoke').on('click', addClicked);
+  function addClicked() {
+    var userJoke = {                // creating the user joke object
+      whoseJoke: $('#whoseJoke').val(),
+      jokeQuestion: $('#jokeQuestion').val(),
+      punchLine: $('#punchLine').val()
+    };
+
+
+    $.ajax({
+  type: 'POST',
+  url: '/jokes/sendJoke',
+  data: userJoke,
+  success: function(data){
+  },
+  error: function(error){
+  }
+});
+
+$.ajax({
+   type: 'GET',
+   url: '/jokes/getJoke',
+   success: function(response) {
+     $('.display').text(response);
+   },
+   error: function(error){
+   }
+ });
+}
 });
